23eec541b728a18fc575478bd9bf5d64ee2eb79c
JinYeongKim <handayoung11@gmail.com>
2022-05-09 08:12:53 +0900

    정률할인 정책을 위한 RateDiscountPolicy class 생성 및 test code 작성

---
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,34 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP 10% 할인")
+    void vipDiscount() {
+        //given
+        Member member = new Member(1L, "member", Grade.VIP);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("BASIC 할인 X")
+    public void noDiscount() throws Exception {
+        //given
+        Member member = new Member(1L, "member", Grade.BASIC);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        assertThat(discount).isEqualTo(0);
+    }
+}
