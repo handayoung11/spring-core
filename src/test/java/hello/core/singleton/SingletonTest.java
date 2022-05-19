4547802579fd2503cec12dd691b5ca1aeb08bec6
JinYeongKim <handayoung11@gmail.com>
2022-05-20 01:13:47 +0900

    SingletonService test code 작성

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -2,9 +2,10 @@ package hello.core.singleton;
 
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SingletonTest {
     
@@ -19,6 +20,22 @@ public class SingletonTest {
         MemberService memberService2 = appConfig.memberService();
 
         //참조값이 다른 것을 확인. memberService1 != memberService2
-        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        // 생성자의 접근제어자를 private으로 설정하여, 호출 시 컴파일 오류
+        // new SingletonService();
+
+        //1. 조회: 호출할 때마다 같은 객체를 반환
+        SingletonService singletonService1 = SingletonService.getInstance();
+        //2. 조회: 호출할 때마다 같은 객체를 반환
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        //singletonService1 == singletonService2
+        assertThat(singletonService1).isSameAs(singletonService2);
+        singletonService1.logic();
     }
 }
