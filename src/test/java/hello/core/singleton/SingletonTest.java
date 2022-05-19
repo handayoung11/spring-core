98d928c042cd84449a3a2144f809fc1d418943f3
JinYeongKim <handayoung11@gmail.com>
2022-05-19 22:09:30 +0900

    Spring 없는 순수한 DI Container에 대한 test code 작성

---
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,24 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingletonTest {
+    
+    @Test
+    @DisplayName("Spring 없는 순수한 DI Container")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+        //1. 조회: 호출할 때마다 객체를 생성
+        MemberService memberService1 = appConfig.memberService();
+
+        //2. 조회: 호출할 때마다 객체를 생성
+        MemberService memberService2 = appConfig.memberService();
+
+        //참조값이 다른 것을 확인. memberService1 != memberService2
+        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+    }
+}
