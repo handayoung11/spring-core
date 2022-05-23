3ac4fb2948dcf0c6d4a4d6cbb668c2046f42a319
JinYeongKim <handayoung11@gmail.com>
2022-05-23 16:26:02 +0900

    @Configuration annotation이 없는 경우 클래스 구성 정보 및 싱글톤 관리 여부 test code 작성

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -1,12 +1,18 @@
 package hello.core.singleton;
 
 import hello.core.AppConfig;
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberMemoryRepository;
 import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -30,5 +36,51 @@ public class ConfigurationSingletonTest {
         AppConfig bean = ac.getBean(AppConfig.class);
         System.out.println("bean = " + bean.getClass());
         //출력결과: bean = class hello.core.AppConfig$$EnhancerBySpringCGLIB$$3fe39412
+    }
+
+    @Test
+    void configWithoutAnnotationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfigWithoutConf.class);
+        AppConfigWithoutConf bean = ac.getBean(AppConfigWithoutConf.class);
+        System.out.println("bean = " + bean.getClass());
+        //출력결과: bean = class hello.core.singleton.AppConfigWithoutConf
+    }
+
+    @Test
+    void configWithoutAnnotationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfigWithoutConf.class);
+        MemberServiceImpl memberService = ac.getBean(MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean(OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean(MemberRepository.class);
+
+        //모두 같은 인스턴스를 참조하지 않는다.
+        assertThat(memberService.getMemberRepository()).isNotSameAs(memberRepository);
+        assertThat(orderService.getMemberRepository()).isNotSameAs(memberRepository);
+    }
+}
+
+class AppConfigWithoutConf {
+
+    @Bean
+    public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    @Bean
+    public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
+        return new MemberMemoryRepository();
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
 }
