03134d12219ea6ee4d0c361d4155b7c96b2f172e
JinYeongKim <handayoung11@gmail.com>
2022-05-23 16:25:30 +0900

    AppConfig class 구성정보 출력 확인 test code 작성

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -23,4 +23,12 @@ public class ConfigurationSingletonTest {
         assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
     }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+        System.out.println("bean = " + bean.getClass());
+        //출력결과: bean = class hello.core.AppConfig$$EnhancerBySpringCGLIB$$3fe39412
+    }
 }
