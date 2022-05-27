2289cc72523a2789120e555eb899e5307f6ef4aa
JinYeongKim <handayoung11@gmail.com>
2022-05-27 09:08:46 +0900

    AutoAppConfig도 Component Scan 및 @Bean Singleton 관리되도록 @Configuration 추가

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
+@Configuration
 @ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, value = Configuration.class))
 public class AutoAppConfig {
 }
