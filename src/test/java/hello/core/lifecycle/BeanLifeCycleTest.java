516cab59993be6a7772f77a6478fccd0ef86483d
JinYeongKim <handayoung11@gmail.com>
2022-06-23 08:05:15 +0900

    @PreDestroy, @PostConstruct를 통한 초기화 및 소멸 메서드 호출 테스트

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -18,7 +18,7 @@ public class BeanLifeCycleTest {
 
 @Configuration
 class LifeCycleConfig {
-    @Bean(initMethod = "init", destroyMethod = "close")
+    @Bean
     public NetworkClient networkClient() {
         NetworkClient networkClient = new NetworkClient();
         networkClient.setUrl("http://hello-spring.dev");
