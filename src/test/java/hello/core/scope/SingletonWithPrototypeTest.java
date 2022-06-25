862c5ea50473670171dfcb0e71fd7098050f2b40
JinYeongKim <handayoung11@gmail.com>
2022-06-25 09:05:45 +0900

    Prototype Bean test code 작성

---
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
@@ -0,0 +1,49 @@
+package hello.core.scope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+public class SingletonWithPrototypeTest {
+
+    @Test
+    void prototypeFind() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean bean1 = ac.getBean(PrototypeBean.class);
+        bean1.addCount();
+        assertSame(bean1.getCount(), 1);
+
+        PrototypeBean bean2 = ac.getBean(PrototypeBean.class);
+        bean2.addCount();
+        assertSame(bean2.getCount(), 1);
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init " + this);
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
