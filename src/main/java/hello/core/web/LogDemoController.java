666c5926c01db31e24296524a5f6866dfadea56c
JinYeongKim <handayoung11@gmail.com>
2022-06-29 07:44:38 +0900

    MyLogger를 확인해보기 위한 LogDemoController와 LogDemoService 생성

---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,28 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import hello.core.logdemo.LogDemoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @ResponseBody
+    @RequestMapping("log-demo")
+    public String logDemo(HttpServletRequest request) {
+        String url = request.getRequestURL().toString();
+        myLogger.setRequestURL(url);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
